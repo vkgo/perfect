42033346de4c0f889e78178259ef8d7a5a038623
vkgo <hwjho@qq.com>
2022-09-03 20:02:36 +0800

    1st

---
+++ setup.py
@@ -0,0 +1,34 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Install Making Language models better few-shot learners."""
+import setuptools
+
+
+def setup_package():
+    setuptools.setup(
+        name='fewshot',
+        version='0.0.1',
+        description='PERFECT: Prompt-free and Efficient Few-shot Learning with Language Models',
+        author='Rabeeh Karimi Mahabadi',
+        license='MIT License',
+        packages=setuptools.find_packages(
+            exclude=['docs', 'tests', 'scripts', 'examples']),
+        install_requires=[
+        ],
+        classifiers=[
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.7',
+        ],
+        keywords='nlp machinelearning',
+    )
+
+
+if __name__ == '__main__':
+    setup_package()
